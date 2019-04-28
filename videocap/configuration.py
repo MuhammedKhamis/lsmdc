ebbb432781239d318ed19977dea7fe04a212e5c4
MuhammedKhamis <muhammedessam10@gmail.com>
2019-04-28 15:59:55 +0200

    add pretrained model path

--- videocap/configuration.py
+++ videocap/configuration.py
@@ -55,5 +55,5 @@ class TrainConfig(object):
         self.steps_per_evaluate = 100
         self.train_tag = 'RET'
 
-        self.load_from_ckpt = None #'/content/checkpoint/retmodel.ckpt'
+        self.load_from_ckpt = '/content/checkpoint/retmodel.ckpt'
         self.print_evaluate = False
